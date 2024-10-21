71accc5cec69239b5d56facd1eee2aafa930ddef
musaucrea <musaucreative@gmail.com>
2024-10-21 02:33:46 -0500

    updates

--- foti-backend/models/routes/models/models/Paper.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const PaperSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  author: { type: String, required: true },
-  publishedDate: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model('Paper', PaperSchema);
